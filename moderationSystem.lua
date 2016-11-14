fe9ca5a3867d08b39371a5f86445f7bbf0abef88
Micah Ayers <FluffyStreusel@users.noreply.github.com>
2016-11-14 17:36:24 -0600

    Update moderationSystem.lua

--- moderationSystem.lua
+++ moderationSystem.lua
@@ -2,6 +2,8 @@
 	Moderator Commands ~ Stormersoul
 --]]
 
+local Debris = game:GetService'Debris'
+
 local Administrators = {
 	["Stormersoul"] = true,
 	["caden2hip"] = true,
@@ -18,18 +20,18 @@ local Administrators = {
 	["IICyanTheDiamondII"] = true
 }
 
-game.Players.PlayerAdded:connect(function(player2)
-	if Administrators[player2.Name] then
+game.Players.PlayerAdded:connect(function(plr)
+	if Administrators[plr.Name] then
 		local hint = Instance.new("Hint")
-		hint.Text = player2.Name .. " has joined and is an administrator."
+		hint.Text = plr.Name .. " has joined. They are an administrator."
 		hint.Parent = workspace
-		game:GetService("Debris"):AddItem(hint, 6)
+		Debris(hint, 6)
 	end
 end)
 
-function findPlayer(name)
+function findPlayer(uName)
 	for _, player in ipairs(game.Players:GetPlayers()) do
-		if player.Name:lower() == name:lower() then
+		if player.Name:lower() == uName:lower() then
 			return player
 		end
 	end
@@ -37,9 +39,9 @@ end
 
 function onChatted(message, player)
 	if message:sub(1, 5) == "/kill" and Administrators[player.Name] then
-		local victim = findPlayer(message:sub(7))
-		if victim and victim.Character then
-			victim.Character:BreakJoints()
+		local usr = findPlayer(message:sub(7))
+		if usr and usr.Character then
+			usr.Character:BreakJoints()
 		end
 	end
 	if message:sub(1, 5) == "/warn" and Administrators[player.Name] then
